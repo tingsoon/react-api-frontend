d4aaf3e0169841c25a6d5d84e63bc789a69262ee
tingsoon <angts8@hotmail.com>
2020-11-30 11:06:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tgrzc5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tgrzc5)
---
+++ containers/master/master.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+export default class Master extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      users: [],
+      error: null
+    }
+    // https://jsonplaceholder.typicode.com/users?_sort=name&_order=asc
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>Contacts</h1>
+      </div>
+    );
+  }
+}
